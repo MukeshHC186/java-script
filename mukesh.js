d527ea16456a770aa6cabff113342b98e2589d39
mukesh kumar jat <mjatsuper01@gmail.com>
2024-03-11 17:06:40 +0530

    first java introduction

---
+++ mukesh.js
@@ -0,0 +1,19 @@
+let a;
+a=10;
+a=20;
+a=30;
+console.log(a)
+const b=50;
+var c=150;
+c=200;
+c=309;
+console.log(b);
+console.log(c)
+let data=1000;
+if(data===1000){
+    a=500;
+}
+else{
+    console.log("not applicable")
+}
+console.log(a, "line no 19")
