63ef0d3c4a70e3a828b92075a66c8d31e0abed45
mukesh kumar jat <mjatsuper01@gmail.com>
2024-03-12 17:45:19 +0530

    function

---
+++ 12-03-2024/index.js
@@ -0,0 +1,54 @@
+// b is local variable and x,a is global variable
+
+
+// var x=20;
+// a=5
+// function mkd(b) {
+// console.log("hello from mukesh")
+// console.log(a+b)
+// console.log(x)
+// }
+// mkd(3)
+// console.log(x+a)
+
+
+//array
+
+// let personinfo=[
+//     {
+//         firstname:"mukesh",
+//         lname:"jat",
+//         mobile:8441925100,
+//         city:"jaipur"
+//     },
+//     {
+//         firstname:"lokesh",
+//         lname:"yadav",
+//         mobile:8441925100,
+//         city:"udaipur"
+//     } ,
+//     {
+//         firstname:"ramesh",
+//         lname:"jat",
+//         mobile:8441925100,
+//         city:"jodhpur"
+//     } ,
+//     {
+//         firstname:"manish",
+//         lname:"jat",
+//         mobile:8441925100,
+//         city:"dholpur"
+//     }
+// ]
+// console.log(personinfo[1])
+
+//function
+
+let x=100;
+function mukesh(a,b){
+console.log("hello from manish")
+return a+b
+console.log(x)
+}
+let value=mukesh(1,9)
+console.log(value)
