2c8872b7f9339d5e39823e0cea7e0ef55634e5ee
mukesh kumar jat <mjatsuper01@gmail.com>
2024-03-12 12:36:32 +0530

    mkd

--- datatype.js
+++ datatype.js
@@ -1,3 +1,6 @@
+// --------Q.1---------
+
+
 // ----primitives (value type)-------
 // ------1.number----------
 let a=10;
@@ -40,5 +43,29 @@ console.log(typeof c)
 function mk (a){
     console.log(a)
 }
+
+
+// ----------Q.2------------
+//java script is a single theraded programming (scripting) language
+
+//----------Q.3------------
+//An array is a collection of items of same data type stored at contiguous memory locations
+
+//---------Q.4------------
+//objects are basic concepts of Object Oriented Programming (OOPs) that are used to represent real-world concepts and entities. 
+
+//------Q.5-------------
+//        Primitive data structure                                    	     Non-primitive data structure
+//Primitive data structure is a kind of data structure                Non-primitive data structure is a type of data structure
+//      that stores the data ofonly one type.                               that can store the data of more than one type.
+
+//--------------Q.6--------------
+//key words for declare variables in js is 
+//1.let (block scope)
+//2.var (global scope)
+//3.const (block scope)
+
+	
+
 
 
