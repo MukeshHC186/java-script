652facf0ad3dd52623bd292a973ae461d4b45a66
mukesh kumar jat <mjatsuper01@gmail.com>
2024-03-18 12:30:12 +0530

    task completed

--- 12-03-2024/index.js
+++ 12-03-2024/index.js
@@ -44,11 +44,30 @@
 
 //function
 
-let x=100;
-function mukesh(a,b){
-console.log("hello from manish")
-return a+b
-console.log(x)
-}
-let value=mukesh(1,9)
-console.log(value)
+// let x=100;
+// function mukesh(a,b){
+// console.log("hello from manish")
+// return a+b
+// console.log(x)
+// }
+// let value=mukesh(1,9)
+// console.log(value)
+// Define two numbers
+let num1 = 50;
+let num2 = 10;
+
+// Add the numbers
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+let modula = num1 % num2;
+
+// Display the result
+console.log("The sum of", num1, "and", num2, "is:", sum);
+console.log("The sub of", num1, "and", num2, "is:", sub);
+console.log("The mul of", num1, "and", num2, "is:", mul);
+console.log("The div of", num1, "and", num2, "is:", div);
+console.log("The modula of", num1, "and", num2, "is:", modula);
+
+
