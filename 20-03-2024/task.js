53f935386faa8d0beeb5ee8e4301597f7c7c5891
mukesh kumar jat <mjatsuper01@gmail.com>
2024-03-20 17:36:52 +0530

    20-03-24 task completed

---
+++ 20-03-2024/task.js
@@ -0,0 +1,22 @@
+//Queation.1
+
+let a=10;
+let b=20;
+let c=30;
+(a>b && a>c)?console.log("a is gretest"):(b>a && b>c)?console.log("b is gretest"):console.log("c is grestest")
+
+//Queation.2
+
+let num=0;
+num>0?console.log("num is positive"):num==0?console.log("0 is either negative or positive"):console.log("num is negative")
+
+//Queation.3
+
+let loggedin="true";
+let result;
+loggedin=="true"?console.log("Welcome back"):console.log("Please log in")
+
+//Queation.4
+
+let dayofweek="Sunday";
+dayofweek=="Saturday"?console.log("Not Weeakend"):dayofweek=="Sunday"?console.log("Weekend"):console.log("Working day")
