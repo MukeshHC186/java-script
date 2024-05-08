f7fb74b18114f1b5e87a30e7381ba85cb210fb2a
mukesh kumar jat <mjatsuper01@gmail.com>
2024-05-08 17:58:33 +0530

    task completed

---
+++ 07-05-2024/index.js
@@ -0,0 +1,20 @@
+// <---------Q.1----------->
+const numbers=[1,2,3,4,5,6,7,8,9,10]
+const twotable=numbers.map(function(items){
+    return items*2
+});
+console.log(twotable);
+
+// <---------Q.2----------->
+const number=[1,2,3,4,5,6,7,8,9,10]
+const onetoten=number.forEach(function(num){
+console.log(num);
+    
+});
+// <---------Q.3----------->
+const Details=[
+    {name:'devid',city:'jaipur'}
+    ,{name:'rahul',city:'jodhpur'}
+,{name:'ram',city:'delhi'}
+,{name:'vivek',city:'mumbai'}
+]
