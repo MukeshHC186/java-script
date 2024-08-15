bb132d75cc023244df5acaf6919cab1ccd71399c
mukesh jat <mjatsuper01@gmail.com>
2024-08-15 09:30:07 +0530

    json server setup

---
+++ java-script-practice/loops.js
@@ -0,0 +1,61 @@
+//---------loops----------
+//------1. while loop------
+
+// let i = 0;
+// while (i < 5) {
+//     console.log(i);
+//     i++;
+// }
+
+
+//------2. for loop------
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+//------3. forEach------
+// let arr = [1, 2, 3, 4, 5];
+// arr.forEach(function (item) {
+//     item=item+1
+//     console.log(item);
+// })  
+
+//-------4.map-----------
+// let arr = [1, 2, 3, 4, 5];
+// let newArr = arr.map(function (item) {
+//     item=item*2
+//     console.log(item);
+// })
+
+//-------5.filter-------
+// let arr = [1, 2, 3, 4, 5];
+// let newArr = arr.filter(function (item) {
+//     return item > 3
+// })
+// console.log(newArr);
+
+//-------6.reduce-------
+// let arr = [1, 2, 3, 4, 5];
+// let sum = arr.reduce(function (a, item) {
+//     return a + item
+// })
+// console.log(sum);
+
+//-------7.some-------
+// let arr = [1, 2, 3, 4, 5];
+// let isEven = arr.some(function (item) {
+//     return item % 2 === 0
+// })
+// console.log(isEven);
+
+//-------8.find----------
+// let arr = [1, 2, 3, 4, 5];
+// let findItem = arr.find(function (item) {
+//     return item > 3
+// })
+// console.log(findItem);
+
+
+
+
+
