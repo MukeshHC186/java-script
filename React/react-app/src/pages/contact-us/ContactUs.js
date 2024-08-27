3ded0135236501247451194d7285fc93114a58a7
mukesh kumar jat <mjatsuper01@gmail.com>
2024-08-27 13:05:58 +0530

    url change

--- React/react-app/src/pages/contact-us/ContactUs.js
+++ React/react-app/src/pages/contact-us/ContactUs.js
@@ -1,6 +1,55 @@
-const ContactUs=()=>{
-    return(
-        <div className="contact-us">ContactUs</div>
-    )
-}
-export default ContactUs
+import './ContactUs.css'
+const contactUsObj = {
+    heading:"Contact Us",
+  form: [
+    {
+      Id: "name",
+      type: "input",
+      subType: "text",
+      label: "Name:- ",
+      placeholder: "Enter your name",
+      name: "name",
+    },
+
+    {
+      Id: "email",
+      type: "input",
+      subType: "email",
+      label: "Email:- ",
+      placeholder: "Enter your email",
+      name: "email",
+    },
+
+    {
+      Id: "mobile",
+      type: "input",
+      subType: "mobile",
+      label: "Mobile:- ",
+      placeholder: "Enter your mobile number",
+      name: "mobile",
+    },
+    {
+      Id: "message",
+      type: "textarea",
+      label: "Message:- ",
+      name: "message",
+    },
+  ],
+};
+const ContactUs = () => {
+  return <div className="contact-us">
+    <h1>{contactUsObj.heading}</h1>
+    <form className="form">
+        {contactUsObj.form.map(ele=>{
+            return <div key={ele.Id}>
+                <label>{ele.label}</label>
+                <input type={ele.type} name={ele.name} placeholder={ele.placeholder}required />
+            </div>
+        })}
+        <div className='btn_div1'>
+        <button>Submit</button>
+        </div>
+    </form>
+  </div>;
+};
+export default ContactUs;
