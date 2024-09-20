9ba733e67d48423e72ab16be7af830758844d661
mukesh kumar jat <mjatsuper01@gmail.com>
2024-09-20 17:57:58 +0530

    x

--- React/x-clone/src/components/layout/aside/Aside.js
+++ React/x-clone/src/components/layout/aside/Aside.js
@@ -3,61 +3,59 @@ import { PiXLogo } from "react-icons/pi";
 import { MdHomeFilled } from "react-icons/md";
 import { CiSearch } from "react-icons/ci";
 import { IoMdNotificationsOutline } from "react-icons/io";
-import { MdEmail } from "react-icons/md";
+import { MdOutlineMail } from "react-icons/md";
 import { VscDiffIgnored } from "react-icons/vsc";
 import { FaRegBookmark } from "react-icons/fa";
 import { BsFillPeopleFill } from "react-icons/bs";
 import { MdOutlineFlashOn } from "react-icons/md";
 import { IoPersonOutline } from "react-icons/io5";
 import { CiCircleMore } from "react-icons/ci";
+import { BsThreeDots } from "react-icons/bs";
+import { MdOutlineVerified } from "react-icons/md";
 const asideData=[
     {
-        logo:<PiXLogo />,
+        logo:<PiXLogo className='bg-transparent'/>,
         name:""
     } ,
     {
-        logo:<MdHomeFilled />,
+        logo:<MdHomeFilled className='bg-transparent' />,
         name:"Home"
     } ,
     {
-        logo:<CiSearch />,
+        logo:<CiSearch className='bg-transparent'/>,
         name:"Explore"
     },
     {
-        logo:<IoMdNotificationsOutline />,
+        logo:<IoMdNotificationsOutline className='bg-transparent'/>,
         name:"Notification"
     },
     {
-        logo:<MdEmail />,
+        logo:<MdOutlineMail className='bg-transparent'/>,
         name:"Message"
     },
     {
-        logo:<VscDiffIgnored />,
+        logo:<VscDiffIgnored className='bg-transparent'/>,
         name:"Grok"
     },
     {
-        logo:<PiXLogo />,
+        logo:<PiXLogo className='bg-transparent'/>,
         name:"Premium"
     },
     {
-        logo:<FaRegBookmark />,
+        logo:<FaRegBookmark className='bg-transparent'/>,
         name:"Bookmarks"
     },
     {
-        logo:<BsFillPeopleFill />,
+        logo:<BsFillPeopleFill className='bg-transparent'/>,
         name:"Communities"
     },
     {
-        logo:<MdOutlineFlashOn />,
+        logo:<MdOutlineFlashOn className='bg-transparent'/>,
         name:"Verified Orgs"
     },
     {
-        logo:<IoPersonOutline />,
+        logo:<IoPersonOutline className='bg-transparent'/>,
         name:"Profile"
-    },
-    {
-        logo:<CiCircleMore />,
-        name:"More"
     }
 ]
 const Aside=()=>{
@@ -67,11 +65,29 @@ const Aside=()=>{
             asideData.map(el=>{
                 return(
                     <div key={el.name}>
-                        <div className='logo_div'>{el.logo}<p>{el.name}</p></div>
+                        <div className='logo_div'><button className='btn1'>{el.logo}{el.name}</button></div>
                     </div>
                 )
             })
         }
+        <div class='logo_div1 dropup '><button class='btn1'  type="button" data-bs-toggle="dropdown" aria-expanded="false"><CiCircleMore className='bg-transparent'/>More</button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+               <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+        <button className='btn mt-1'>Post</button>
+        <div className='loginProfile'>
+            <div className='w-100% mt-5 d-flex gap-4' >
+                <img style={{width:"50px",height:"50px",borderRadius:"50%"}} src='https://avatars.githubusercontent.com/u/158014471?v=4' alt='mk'></img>
+                <div className='loginProfile_details'>
+                    <b>MUKESH KUMAR JAT <MdOutlineVerified /></b>
+                    <p>mjatsuper@gmail.com</p>
+                </div>
+                <div className='mt-2'><b><BsThreeDots /></b></div>
+            </div>
+        </div>
         </div>
         );
 }
--- React/x-clone/src/components/layout/main/Main.js
+++ React/x-clone/src/components/layout/main/Main.js
@@ -1,8 +1,26 @@
 import '../main/Main.scss'
+import { FaEarthAfrica } from "react-icons/fa6";
+import { CiImageOn } from "react-icons/ci";
+import { MdOutlineGifBox } from "react-icons/md";
 const Main=()=>{
     return(
         <div className="Main">
-            
+            <div className='Main_foryou'>
+                <span>For you</span>
+                <span>Following</span>
+            </div>
+            <div className='Main_whathappen'>
+            <img style={{width:"50px",height:"50px",borderRadius:"50%"}} src='https://avatars.githubusercontent.com/u/158014471?v=4' alt='mk'></img>
+            <input type='text' placeholder='What is happening?!'></input><br></br>
+            <span className='who_reply'><FaEarthAfrica className='text-primary'/>  Everyone can reply</span><hr style={{width:"80%",margin:"10px auto"}}></hr>
+           <div className='formatting_icon_main'>
+            <div className='formatting_icons'>
+                <CiImageOn />
+                <MdOutlineGifBox />
+
+                </div>
+           </div>
+            </div>
         </div>
         );
 }
