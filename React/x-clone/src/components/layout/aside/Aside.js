6c2a99c4892d40b8accd0d312971f36fc497d5bb
mukesh kumar jat <mjatsuper01@gmail.com>
2024-09-24 17:56:03 +0530

    x

--- React/x-clone/src/components/layout/aside/Aside.js
+++ React/x-clone/src/components/layout/aside/Aside.js
@@ -1,4 +1,4 @@
-import '../aside/Aside.scss'
+import "../aside/Aside.scss";
 import { PiXLogo } from "react-icons/pi";
 import { MdHomeFilled } from "react-icons/md";
 import { CiSearch } from "react-icons/ci";
@@ -12,83 +12,140 @@ import { IoPersonOutline } from "react-icons/io5";
 import { CiCircleMore } from "react-icons/ci";
 import { BsThreeDots } from "react-icons/bs";
 import { MdOutlineVerified } from "react-icons/md";
-const asideData=[
-    {
-        logo:<PiXLogo className='bg-transparent'/>,
-        name:""
-    } ,
-    {
-        logo:<MdHomeFilled className='bg-transparent' />,
-        name:"Home"
-    } ,
-    {
-        logo:<CiSearch className='bg-transparent'/>,
-        name:"Explore"
-    },
-    {
-        logo:<IoMdNotificationsOutline className='bg-transparent'/>,
-        name:"Notification"
-    },
-    {
-        logo:<MdOutlineMail className='bg-transparent'/>,
-        name:"Message"
-    },
-    {
-        logo:<VscDiffIgnored className='bg-transparent'/>,
-        name:"Grok"
-    },
-    {
-        logo:<PiXLogo className='bg-transparent'/>,
-        name:"Premium"
-    },
-    {
-        logo:<FaRegBookmark className='bg-transparent'/>,
-        name:"Bookmarks"
-    },
-    {
-        logo:<BsFillPeopleFill className='bg-transparent'/>,
-        name:"Communities"
-    },
-    {
-        logo:<MdOutlineFlashOn className='bg-transparent'/>,
-        name:"Verified Orgs"
-    },
-    {
-        logo:<IoPersonOutline className='bg-transparent'/>,
-        name:"Profile"
-    }
-]
-const Aside=()=>{
-    return(
-        <div className="aside">
-          {
-            asideData.map(el=>{
-                return(
-                    <div key={el.name}>
-                        <div className='logo_div'><button className='btn1'>{el.logo}{el.name}</button></div>
-                    </div>
-                )
-            })
-        }
-        <div class='logo_div1 dropup '><button class='btn1'  type="button" data-bs-toggle="dropdown" aria-expanded="false"><CiCircleMore className='bg-transparent'/>More</button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-               <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-        </div>
-        <button className='btn mt-1'>Post</button>
-        <div className='loginProfile'>
-            <div className='w-100% mt-5 d-flex gap-4' >
-                <img style={{width:"50px",height:"50px",borderRadius:"50%"}} src='https://avatars.githubusercontent.com/u/158014471?v=4' alt='mk'></img>
-                <div className='loginProfile_details'>
-                    <b>MUKESH KUMAR JAT <MdOutlineVerified /></b>
-                    <p>mjatsuper@gmail.com</p>
-                </div>
-                <div className='mt-2'><b><BsThreeDots /></b></div>
+import { RiFileList2Fill } from "react-icons/ri";
+import { FaMoneyBills } from "react-icons/fa6";
+import { LuArrowUpRightSquare } from "react-icons/lu";
+import { MdPunchClock } from "react-icons/md";
+import { SlSettings } from "react-icons/sl";
+import { TbSpaces } from "react-icons/tb";
+const asideData = [
+  {
+    logo: <PiXLogo className="bg-transparent" />,
+    name: "",
+  },
+  {
+    logo: <MdHomeFilled className="bg-transparent" />,
+    name: "Home",
+  },
+  {
+    logo: <CiSearch className="bg-transparent" />,
+    name: "Explore",
+  },
+  {
+    logo: <IoMdNotificationsOutline className="bg-transparent" />,
+    name: "Notification",
+  },
+  {
+    logo: <MdOutlineMail className="bg-transparent" />,
+    name: "Message",
+  },
+  {
+    logo: <VscDiffIgnored className="bg-transparent" />,
+    name: "Grok",
+  },
+  {
+    logo: <PiXLogo className="bg-transparent" />,
+    name: "Premium",
+  },
+  {
+    logo: <FaRegBookmark className="bg-transparent" />,
+    name: "Bookmarks",
+  },
+  {
+    logo: <BsFillPeopleFill className="bg-transparent" />,
+    name: "Communities",
+  },
+  {
+    logo: <MdOutlineFlashOn className="bg-transparent" />,
+    name: "Verified Orgs",
+  },
+  {
+    logo: <IoPersonOutline className="bg-transparent" />,
+    name: "Profile",
+  },
+];
+const moreData = [
+  {
+    logo: <RiFileList2Fill />,
+    name: "Lists",
+  },
+  {
+    logo: <FaMoneyBills />,
+    name: "Monetization",
+  },
+  {
+    logo: <LuArrowUpRightSquare />,
+    name: "Ads",
+  },
+  {
+    logo: <MdPunchClock />,
+    name: "Jobs",
+  },
+  {
+    logo: <TbSpaces />,
+    name: "Create your Space",
+  },
+  {
+    logo: <SlSettings />,
+    name: "Sattings and privace",
+  },
+];
+const Aside = () => {
+  return (
+    <div className="aside">
+      {asideData.map((el) => {
+        return (
+          <div key={el.name}>
+            <div className="logo_div">
+              <button className="btn1">
+                {el.logo}
+                {el.name}
+              </button>
             </div>
-        </div>
-        </div>
+          </div>
         );
-}
-export default Aside
+      })}
+      <div class="logo_div1 dropup ">
+        <button
+          class="btn1"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <CiCircleMore className="bg-transparent" />
+          More
+        </button>
+            <ul class="dropdown-menu">
+                {moreData.map(el=>{
+            return(
+                <li><a class="dropdown-item" href="#">{el.logo}  {el.name}</a></li>
+            )})
+        }
+            </ul>
+        
+      </div>
+      <button className="btn mt-1">Post</button>
+      <div className="loginProfile">
+        <div className="w-100% mt-5 d-flex gap-4">
+          <img
+            style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+            src="https://avatars.githubusercontent.com/u/158014471?v=4"
+            alt="mk"
+          ></img>
+          <div className="loginProfile_details">
+            <b>
+              MUKESH KUMAR JAT <MdOutlineVerified className="text-primary fs-5"/>
+            </b>
+            <p>mjatsuper@gmail.com</p>
+          </div>
+          <div className="mt-2">
+            <b>
+              <BsThreeDots />
+            </b>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Aside;
--- React/x-clone/src/components/layout/main/Main.js
+++ React/x-clone/src/components/layout/main/Main.js
@@ -9,17 +9,29 @@ const Main=()=>{
                 <span>For you</span>
                 <span>Following</span>
             </div>
-            <div className='Main_whathappen'>
-            <img style={{width:"50px",height:"50px",borderRadius:"50%"}} src='https://avatars.githubusercontent.com/u/158014471?v=4' alt='mk'></img>
-            <input type='text' placeholder='What is happening?!'></input><br></br>
-            <span className='who_reply'><FaEarthAfrica className='text-primary'/>  Everyone can reply</span><hr style={{width:"80%",margin:"10px auto"}}></hr>
-           <div className='formatting_icon_main'>
-            <div className='formatting_icons'>
-                <CiImageOn />
-                <MdOutlineGifBox />
-
+            <div className='Main_whathappen d-flex '>
+                <div className='Main_whathappen_img '>
+                  <img style={{width:"50px",height:"50px",borderRadius:"50%"}} src='https://avatars.githubusercontent.com/u/158014471?v=4' alt='mk'></img>
                 </div>
-           </div>
+                <div className='formatting_icon_main text-primary '>
+                   <input type='text' placeholder='What is happening?!'></input><br></br>
+                   <span className='who_reply'><FaEarthAfrica className='text-primary'/>  Everyone can reply</span><hr style={{width:"80%",margin:"10px auto"}}></hr>
+                   <div className=' d-flex justify-content-between ms-3'>
+                   <div className='fs-5 d-flex justify-content-evenly gap-3'>
+                    <CiImageOn />
+                   <MdOutlineGifBox />
+                   <MdOutlineGifBox />
+                   <MdOutlineGifBox />
+                   <MdOutlineGifBox />
+                   <MdOutlineGifBox />
+                   </div>
+                   <div><button className='text-white bg-primary'>Post</button></div>
+                   </div>
+               </div>
+            </div>
+            <div className='text-center p-4 text-primary border border-0 border-primary-subtle'>Show 35 Posts</div>
+            <div>
+                
             </div>
         </div>
         );
